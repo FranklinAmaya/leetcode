4a3b4b19ba70ad2211313033ca7827d80c3294f5
franklinamaya <franklinamaya>
2022-10-18 15:27:47 -0400

    Solution for Min cost to connect all points

--- java_solutions/WaterDistributionVillage.java
+++ java_solutions/WaterDistributionVillage.java
@@ -3,14 +3,74 @@ import java.util.*;
 // https://leetcode.com/problems/optimize-water-distribution-in-a-village/
 public class WaterDistributionVillage {
   public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+    // add virtual edges to represent wells as pipes
+    PriorityQueue<int[]> pipeCosts = new PriorityQueue<>((a, b) -> a[2] - b[2]);
 
-    return 0;
+    for (int i = 0; i < wells.length; i++) {
+      pipeCosts.add(new int[] { 0, i + 1, wells[i] });
+    }
+
+    // add the rest of pipes
+    for (int i = 0; i < pipes.length; i++) {
+      pipeCosts.add(pipes[i]);
+    }
+
+    // account for extra virtual node when declaring union find constructor
+    UnionFind uf = new UnionFind(n + 1);
+    int minCost = 0;
+
+    while (!pipeCosts.isEmpty()) {
+      int[] pipe = pipeCosts.poll();
+
+      if (uf.union(pipe[0], pipe[1])) {
+        minCost += pipe[2];
+      }
+    }
+
+    return minCost;
+  }
+
+  class UnionFind {
+    int[] rank;
+    int[] root;
+
+    public UnionFind(int size) {
+      root = new int[size];
+      rank = new int[size];
+
+      for (int i = 0; i < size; i++) {
+        root[i] = i;
+        rank[i] = 1;
+      }
+    }
+
+    public int find(int x) {
+      if (x == root[x])
+        return x;
+
+      // recursively find the parent of x
+      return root[x] = find(root[x]);
+    }
+
+    public boolean union(int x, int y) {
+      int rootX = find(x);
+      int rootY = find(y);
+
+      // if they both have the same root return
+      if (rootX == rootY) {
+        return false;
+      }
+
+      if (rank[rootX] < rank[rootY]) {
+        root[rootY] = rootX;
+      } else if (rank[rootX] > rank[rootY]) {
+        root[rootX] = rootY;
+      } else {
+        root[rootY] = rootX;
+        rank[rootX]++;
+      }
+
+      return true;
+    }
   }
 }
-
-/*
- * n houses in a village
- * for each house you can build a well which costs wells[i-1] or
- * pipe in water from another well and costs pipes[j] = [house1, house2, cost]
- * return the minimum total cost to supply water to all houses
- */
