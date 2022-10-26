6fec7d6bbfd10aed958df10742ff3491eb6c9868
franklinamaya <franklinamaya>
2022-10-25 22:10:28 -0400

    Solution to Cheapest Flights within K Stops

---
+++ java_solutions/CheapestFlights.java
@@ -0,0 +1,38 @@
+class CheapestFlights {
+  final int INF = 0x3f3f3f3f;
+
+  public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+    if (src == dst) {
+      return 0;
+    }
+
+    // initialize graph
+    int[] prev = new int[n];
+    int[] curr = new int[n];
+    for (int i = 0; i < n; i++) {
+      prev[i] = INF;
+      curr[i] = INF;
+    }
+
+    prev[src] = 0;
+
+    // relax edges repeatedly k + 1
+    for (int i = 1; i < k + 2; i++) {
+      curr[src] = 0;
+      // iterate through all the edges
+      for (int[] edges : flights) {
+        int u = edges[0];
+        int v = edges[1];
+        int w = edges[2];
+
+        if (prev[u] < Integer.MAX_VALUE) {
+          curr[v] = Math.min(curr[v], prev[u] + w);
+        }
+      }
+
+      prev = curr.clone();
+    }
+
+    return curr[dst] != INF ? curr[dst] : -1;
+  }
+}
